b99b9dd067d215b1eee80ff71d862c5602800039
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-01-10 13:44:51 -0500

    Finished form input

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Music() {
+  const [audio, setAudio] = useState([]);
+
+  const handleSelect = (e) => {
+    setAudio(e.target.files);
+  }
+
   return (
-    <div>
-      Music
+    <div className='music'>
+      <h3>Add music</h3>
+      <p>Note: Uploaded audio will play in the background as the user reads. Leave blank for no music. If multiple audio files are uploaded, they will be looped in a random order.</p>
+      <div>
+        <label htmlFor="file">Choose .mp3 files:</label>
+        <input type="file" id="music-file" accept=".mp3" name="file" multiple onChange={handleSelect}/>
+        <br />
+      </div>
     </div>
   )
 }
--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function WelcomeScreen() {
+  const [welcomeTitle, setWelcomeTitle] = useState("");
+  const [welcomeButton, setWelcomeButton] = useState("");
+
   return (
-    <div>
-      Welcome Screen
+    <div className='welcome'>
+      <h3>Add Welcome Screen</h3>
+      <p>Set Welcome Screen Title</p>
+      <input type="text" value={welcomeTitle} onChange={(e) => setWelcomeTitle(e.target.value)} />
+      <p>Set Start Button Text</p>
+      <input type="text" value={welcomeButton} onChange={(e) => setWelcomeButton(e.target.value)} />
+      <hr />
     </div>
   )
 }
