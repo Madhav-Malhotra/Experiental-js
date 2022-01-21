71d584652c74d90ae159bfad7bb40377935c1d23
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-01-21 09:22:16 -0500

    Deployed to Firebase

--- src/components/Generate.js
+++ src/components/Generate.js
@@ -6,6 +6,7 @@ export default function Generate(props) {
     <div className='generate'>
       <button onClick={() => generate(props)}>Generate Article</button>
       <p>Note: The article will be downloaded as a HTML document.</p>
+      <p><a target="_blank" href="https://icons8.com/icon/GH1SbXMwPs8K/vr-glasses">Vr Glasses</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkVLWc30wD640m3xeyFWQgx3s-lo_S5UA",
+  authDomain: "experiential-ly.firebaseapp.com",
+  projectId: "experiential-ly",
+  storageBucket: "experiential-ly.appspot.com",
+  messagingSenderId: "67933253872",
+  appId: "1:67933253872:web:87a16fca23f8b8bd861235",
+  measurementId: "G-Z2S08FLN32"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
